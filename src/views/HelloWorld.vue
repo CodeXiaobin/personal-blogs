7fb890d8ed47bd1dd1453353bb4fa3485ff33658
xiaobin <2720367372@qq.com>
2023-05-18 18:41:08 +0800

    init：配置sass

--- src/views/HelloWorld.vue
+++ src/views/HelloWorld.vue
@@ -1,11 +1,15 @@
 <script setup lang="ts"></script>
 
 <template>
-  <h1>HelloWorld</h1>
+  <div>
+    <h1>HelloWorld</h1>
+  </div>
 </template>
 
-<style scoped>
-.read-the-docs {
-  color: #888;
+<style scoped lang="scss">
+div{
+  h1{
+    font-size: 50px;
+  }
 }
 </style>
